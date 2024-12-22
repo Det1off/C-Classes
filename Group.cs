5e0c8e10e636455b243c14eaa0d5c8fc4f93749a
Maxim <maxim.bakal4ul@outlook.com>
2024-12-22 15:57:31 +0200

    Initial commit

---
+++ Group.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class Group
+    {
+        private List<Student> Students;
+        private string GroupName;
+        private string Specialization;
+        private int CourseNumber;
+
+        public Group()
+        {
+            Students = new List<Student>();
+        }
+
+        public void Set_Students(List<Student> students)
+        {
+            Students = students;
+        }
+
+        public List<Student> Get_Students()
+        {
+            return Students;
+        }
+
+        public void Set_GroupName(string groupName)
+        {
+            GroupName = groupName;
+        }
+
+        public string Get_GroupName()
+        {
+            return GroupName;
+        }
+
+        public void Set_Specialization(string specialization)
+        {
+            Specialization = specialization;
+        }
+
+        public string Get_Specialization()
+        {
+            return Specialization;
+        }
+
+        public void Set_CourseNumber(int courseNumber)
+        {
+            CourseNumber = courseNumber;
+        }
+
+        public int Get_CourseNumber()
+        {
+            return CourseNumber;
+        }
+
+        public void AddStudent(Student student)
+        {
+            Students.Add(student);
+        }
+
+        public void ShowAllStudents()
+        {
+            Console.WriteLine($"Group: {GroupName}, Specialization: {Specialization}, Course: {CourseNumber}");
+            foreach (var student in Students)
+            {
+                Console.WriteLine(student.ToString());
+            }
+        }
+
+        public void TransferStudent(Group targetGroup, Student student)
+        {
+            if (Students.Remove(student))
+            {
+                targetGroup.AddStudent(student);
+            }
+        }
+
+        public void ExpelLowestScoringStudent()
+        {
+            if (Students.Count > 0)
+            {
+                var lowestScoringStudent = Students.OrderBy(s => s.Get_TestScores().Average()).FirstOrDefault();
+                if (lowestScoringStudent != null)
+                {
+                    Students.Remove(lowestScoringStudent);
+                }
+            }
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class Student
+    {
+        private string Surname;
+        private string FirstName;
+        private string LastName;
+        private string Birthday;
+        private string Address;
+        private string PhoneNumber;
+
+        private List<int> TestScores;
+        private List<int> CourseworkScores;
+        private List<int> ExamScores;
+
+        public Student() { }
+
+        public Student (string surname, string firstName, string lastName)
+        {
+            Surname = surname;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public void ShowAllData()
+        {
+            Console.WriteLine(ToString());
+            Console.WriteLine(Convert.ToString(Get_TestScores()));
+            Console.WriteLine(Convert.ToString(Get_CourseworkScores()));
+            Console.WriteLine(Convert.ToString(Get_ExamScores()));
+        }
+
+        public override string ToString()
+        {
+            return $"{LastName} {FirstName} {Surname}, DOB: {Birthday}, Address: {Address}, Phone: {PhoneNumber}";
+        }
+
+        public void Set_ExamScores(List<int> examScores)
+        {
+            ExamScores = examScores;
+        }
+
+        public List<int> Get_ExamScores()
+        {
+            return ExamScores;
+        }
+
+        public void Set_CourseworkScores(List<int> courseworkScores)
+        {
+            CourseworkScores = courseworkScores;
+        }
+
+        public List<int> Get_CourseworkScores()
+        {
+            return CourseworkScores;
+        }
+
+        public void Set_TestScores (List<int> testScores)
+        {
+            TestScores = testScores;
+        }
+
+        public List<int> Get_TestScores ()
+        {
+            return TestScores;
+        }
+
+        public void Set_Surname(string surname)
+        {
+            Surname = surname;
+        }
+
+        public string Get_Surname()
+        {
+            return Surname;
+        }
+
+        public void Set_FirstName(string firstName)
+        {
+            FirstName = firstName;
+        }
+
+        public string Get_FirstName()
+        {
+            return FirstName;
+        }
+
+        public void Set_LastName(string lastName)
+        {
+            LastName = lastName;
+        }
+
+        public string Get_LastName ()
+        {
+            return LastName;
+        }
+
+        public void Set_Birthday(string birthday)
+        {
+            Birthday = birthday;
+        }
+
+        public string GetDataTime()
+        {
+            return Birthday;
+        }
+
+        public void Set_Address(string address)
+        {
+            Address = address;
+        }
+
+        public string Get_Address()
+        {
+            return Address;
+        }
+
+        public void Set_PhoneNumber(string phoneNumber)
+        {
+            PhoneNumber = phoneNumber;
+        }
+
+        public string Get_PhoneNumber ()
+        {
+            return PhoneNumber;
+        }
+
+
+
+
+
+
+
+    }
+}
